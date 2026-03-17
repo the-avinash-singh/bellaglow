99e67931a0bd74d1fabcdfbbc89fa8e8db921872
the-avinash-singh <abhinavarya87012@gmail.com>
2026-03-17 16:10:40 +0530

    starting up on backend

---
+++ app/lib/db.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI!;
+
+if (!MONGO_URI) {
+  throw new Error("Please define MONGO_URI");
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      dbName: "bellaglow",
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
