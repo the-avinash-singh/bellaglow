3fb8edefe8f29077f16dfbd3b63ed92f6dfd8dee
Avinash singh <106461441+the-avinash-singh@users.noreply.github.com>
2026-07-05 19:29:14 +0530

    ui change in nav

--- app/Components/Navigationbar.tsx
+++ app/Components/Navigationbar.tsx
@@ -3,34 +3,54 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
 const Navigationbar = () => {
-  const options=[
+  const options = [
     {
-      name:"Skincare",
-      route:"/products/skincare"
+      name: "Skincare",
+      route: "/products/skincare"
     },
     {
-      name:"Fragrance",
-      route:"/products/fragrance"
+      name: "Fragrance",
+      route: "/products/fragrance"
     },
-   
+
   ]
   return (
-    <nav className='sticky top-0 z-50 flex items-center justify-between border-b border-[#e5dcde] bg-background-light/90 px-6 py-4 backdrop-blur-md dark:bg-background-dark/90 dark:border-white/10 lg:px-12'>
-        <div className="flex items-center gap-12">
-<Link className="flex items-center gap-3 text-text-main dark:text-white group" href="/">
-<div className="size-8 text-primary transition-transform group-hover:rotate-12 duration-500">
-<FontAwesomeIcon icon={faVsco} style={{height:30}}/>
-</div>
-<span className={`text-xl font-black tracking-tighter font-display`}>Bella Glow</span>
-</Link>
-<nav className="hidden gap-8 md:flex">
-  {options.map((option,index)=>(
-<Link className={`text-sm font-medium hover:text-primary transition-colors font-display`} key={index+option.name} href={option.route}>{option.name}</Link>
-  ))}
-</nav>
-</div>
+    <nav className="sticky top-0 z-50 flex items-center justify-between border-b border-[#e5dcde] bg-background-light/90 px-6 py-4 backdrop-blur-md dark:bg-background-dark/90 dark:border-white/10 lg:px-12">
 
-</nav>
+      <div className="flex items-center gap-12">
+        <Link
+          className="flex items-center gap-3 text-text-main dark:text-white group"
+          href="/"
+        >
+          <div className="size-8 text-primary transition-transform duration-500 group-hover:rotate-12">
+            <FontAwesomeIcon icon={faVsco} style={{ height: 30 }} />
+          </div>
+          <span className="text-xl font-black tracking-tighter font-display">
+            Bella Glow
+          </span>
+        </Link>
+
+        <div className="hidden gap-8 md:flex">
+          {options.map((option, index) => (
+            <Link
+              key={index + option.name}
+              href={option.route}
+              className="text-sm font-medium font-display transition-colors hover:text-primary"
+            >
+              {option.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <Link href={`https://wa.me/8484006409?text=Hello I am a visiter on Bellaglow.com and I want to inquire about`}
+        target="_blank" 
+        className="text-sm font-medium font-display transition-colors hover:text-primary"
+        >
+        Contact Us
+      </Link>
+
+    </nav>
   )
 }
 
